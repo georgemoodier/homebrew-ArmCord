372a27c0a92de11d9dbff3259d04c0baef6ba79d
georgemoodier <84156418+georgemoodier@users.noreply.github.com>
2023-08-27 20:40:23 +0330

    add intel Mac support

--- Casks/armcord.rb
+++ Casks/armcord.rb
@@ -1,8 +1,14 @@
 cask "armcord" do
-  version "3.2.4"
-  sha256 "a246a1559bcdc4c4243c7544fec7968caf6f6415e0eb442a9a2348332de8a04c"
 
-  url "https://github.com/ArmCord/ArmCord/releases/download/v#{version}/ArmCord-#{version}-arm64.dmg",
+  arch arm: "-arm64", intel: ""
+
+
+  version "3.2.4"
+  sha256 arm: "a246a1559bcdc4c4243c7544fec7968caf6f6415e0eb442a9a2348332de8a04c",
+intel: "1825666f20aa3406b14b405d083d587676f9898d4c55de5353804a8969163459"
+
+
+  url "https://github.com/ArmCord/ArmCord/releases/download/v#{version}/ArmCord-#{version}#{arch}.dmg",
       verified: "github.com/ArmCord/ArmCord/"
   name "armcord"
   desc "Custom lightweight Discord client designed to enhance your experience"
