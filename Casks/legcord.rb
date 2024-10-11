ec50d66822853fa43117d812d2c22346471f9895
georgemoodier <84156418+georgemoodier@users.noreply.github.com>
2024-10-11 23:07:00 +0300

    Update legcord.rb

--- Casks/legcord.rb
+++ Casks/legcord.rb
@@ -3,7 +3,7 @@ cask "legcord" do
   version "1.0.0"
   sha256 "fbc61a35d3eb0d0d398ed3e8fcfd62b6a771da1cb8e484e66a4c67dc4b7aa0a3"
 
-  url "https://github.com/legcord/legcord/releases/download/v#{version}/legcord-#{version}#{arch}.dmg",
+  url "https://github.com/legcord/legcord/releases/download/v#{version}/legcord-#{version}-mac-universal.dmg",
       verified: "github.com/legcord/legcord/"
   name "legcord"
   desc "Custom lightweight Discord client designed to enhance your experience"
